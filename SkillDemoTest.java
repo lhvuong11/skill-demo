51adcb81c32f30e6cee31cf85e41d92284ed0c09
lhvuong11 <lhvuong@ucsd.edu>
2022-02-04 10:32:17 -0800

    lib + starter!

---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+	@Test
+	public void add(){
+		assertEquals(2, SkillDemo.add(1,2)); // Test is failing first!
+	}
+}
